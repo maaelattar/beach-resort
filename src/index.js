aafa21b10afddef6ad4f3fa1da6e7732b5f9908c
maaelattar <41055637+maaelattar@users.noreply.github.com>
2020-07-16 18:36:10 +0200

    Setup context, FeaturedRooms and Loading components

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { RoomProvider } from './context';
 ReactDOM.render(
 	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
+		<RoomProvider>
+			<Router>
+				<App />
+			</Router>
+		</RoomProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
